d7dcaa5eaef2d015840f0739010a567702f2b139
skeny.patel <skeny27@gmail.com>
2022-08-31 23:24:49 +0530

    gin framework basics

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gin
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/validator/v10 v10.11.0
+	github.com/tpkeeper/gin-dump v1.0.0
+)
---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"gin/controller"
+	"gin/middlewares"
+	"gin/service"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+	setupLogOutput()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		err := VideoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
+		}
+	})
+
+	server.Run(":8080")
+}
