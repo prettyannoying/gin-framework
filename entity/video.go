d1c7fbf5d5564380abcf21e8308a532b99a54794
skeny.patel <skeny27@gmail.com>
2022-09-01 12:46:03 +0530

    heroku config

--- entity/video.go
+++ entity/video.go
@@ -7,8 +7,8 @@ type Person struct {
 	Email     string `json:"email" binding:"required,email"`
 }
 type Video struct {
-	Title       string `json:"title" binding:"min=2,max=10" validate:"is-cool"`
-	Description string `json:"description" binding:"max=20"`
+	Title       string `json:"title" binding:"min=2,max=200" validate:"is-cool"`
+	Description string `json:"description" binding:"max=200"`
 	URL         string `json:"url" binding:"required"`
 	Author      Person `json:"author" binding:"required"`
 }
--- server.go
+++ server.go
@@ -26,20 +26,33 @@ func main() {
 	setupLogOutput()
 	server := gin.New()
 
+	server.Static("/css", "./templates/css")
+	server.LoadHTMLGlob("templates/*.html")
 	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, VideoController.FindAll())
-	})
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, VideoController.FindAll())
+		})
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		err := VideoController.Save(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
-		}
-	})
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := VideoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
+			}
+		})
+	}
 
-	server.Run(":8080")
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", VideoController.ShowAll)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	server.Run(":" + port)
 }
